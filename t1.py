93f16733ea64e0132b08216de0827be7081b5f1d
Jeffrey <jhougo7928@gmail.com>
2015-11-10 14:39:42 +0800

    play tk

---
+++ t1.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#                       _oo0oo_
+#                      o8888888o
+#                      88" . "88
+#                      (| -_- |)
+#                      0\  =  /0
+#                    ___/`---'\___
+#                  .' \\|     |// '.
+#                 / \\|||  :  |||// \
+#                / _||||| -:- |||||- \
+#               |   | \\\  -  /// |   |
+#               | \_|  ''\---/''  |_/ |
+#               \  .-\__  '-'  ___/-. /
+#             ___'. .'  /--.--\  `. .'___
+#          ."" '<  `.___\_<|>_/___.' >' "".
+#         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+#         \  \ `_.   \_ __\ /__ _/   .-` /  /
+#     =====`-.____`.___ \_____/___.-`___.-'=====
+#                       `=---='
+#
+#
+#     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#               佛祖保佑         永無BUG
+from tkinter import *
+class GUIDemo(Frame):
+	def __init__(self, master=None):
+		Frame.__init__(self, master)
+		self.grid()
+		self.createWidgets()
+		
+	def createWidgets(self):
+		self.inputText = Label(self)
+		self.inputText["text"] = "Input:"
+		self.inputText.grid(row=0, column=0)
+		self.inputField = Entry(self)
+		self.inputField["width"] = 50
+		self.inputField.grid(row=0, column=1, columnspan=6)
+
+		self.outputText = Label(self)
+		self.outputText["text"] = "Output:"
+		self.outputText.grid(row=1, column=0)
+		self.outputField = Entry(self)
+		self.outputField["width"] = 50
+		self.outputField.grid(row=1, column=1, columnspan=6)
+
+		self.new = Button(self)
+		self.new["text"] = "New"
+		self.new.grid(row=2, column=0)
+		self.load = Button(self)
+		self.load["text"] = "Load"
+		self.load.grid(row=2, column=1)
+		self.save = Button(self)
+		self.save["text"] = "Save"
+		self.save.grid(row=2, column=2)
+		self.encode = Button(self)
+		self.encode["text"] = "Encode"
+		self.encode.grid(row=2, column=3)
+		self.decode = Button(self)
+		self.decode["text"] = "Decode"
+		self.decode.grid(row=2, column=4)
+		self.clear = Button(self)
+		self.clear["text"] = "Clear"
+		self.clear.grid(row=2, column=5)
+		self.copy = Button(self)
+		self.copy["text"] = "Copy"
+		self.copy.grid(row=2, column=6)
+
+		self.displayText = Label(self)
+		self.displayText["text"] = "something happened"
+		self.displayText.grid(row=3, column=0, columnspan=7)
+
+if __name__ == '__main__':
+	root = Tk()
+	app = GUIDemo(master=root)
+	app.mainloop()
